43eca3f1a0623245eccf73b9088c6275a60fc4b3
Cris Franco <cris.franco219@gmail.com>
2021-01-27 16:19:05 -0500

    created get & post req

---
+++ routes/apiRoutes.js
@@ -0,0 +1,39 @@
+
+const fs = require("fs");
+
+module.exports = function (app) {
+    let notes = require("../db/db.json")
+    // GET request
+    app.get("/api/notes", (req, res) => {
+        return res.json(notes)
+    })
+
+    app.get("/api/notes/:id", (req, res) => {
+        const id = req.params.id;
+        let found;
+        notes.forEach(n => {
+            if (id == n.id) {
+                found = n;
+                return res.json(n)
+            }
+        })
+        return res.json(false)
+    })
+    // POST request
+    app.post("/api/notes", (req, res) => {
+        const newNote = req.body;
+        if (notes.length === 0) {
+            newNote.id = 1
+        } else {
+            newNote.id = (notes[notes.length - 1].id + 1);
+        }
+        notes.push(newNote);
+        let jsonNotes = JSON.stringify(notes)
+        fs.writeFile("./db/db.json", jsonNotes, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Success!");
+        })
+        res.json(true)
+    })
